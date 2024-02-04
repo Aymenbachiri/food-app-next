b49a4321783e51d51ac9a6d6b68c9eff30d853d1
Aymenbachiri <aymen.bachiri99@gmail.com>
2024-02-04 10:14:53 +0100

    second commit

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <div className="max-w-[1640px] mx-auto flex justify-between items-center p-4">
       <div>&copy;2024 AYMEN BACHIRI, All Rights Reserved</div>
       <div className="flex items-center gap-4">
-        <a href="/">Portfolio</a>
+        <a href="https://aymen-bachiri.vercel.app/">Portfolio</a>
         <a href="https://www.linkedin.com/in/aymen-bachiri-9442b5287/">
           LinkedIn
         </a>
